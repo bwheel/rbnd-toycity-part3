253f669844219069f65b0f5c4f7fdcd7afa9d8af
bwheel1990@gmail.com <Norby0317>
2016-05-26 21:57:05 -0500

    implemented title search and stock functionality.

--- lib/product.rb
+++ lib/product.rb
@@ -17,13 +17,21 @@ class Product
         @@products << self
     end
     
+    def in_stock?
+        @stock > 0
+    end
+    
     def self.all
-        
-    
-    
         @@products    
     end
     
+    def self.find_by_title(search_title)
+       @@products.select { |prod| prod.title == search_title}[0]
+    end
+    
+    def self.in_stock
+       @@products.select { |prod| prod.stock > 0 }
+    end
     
 end
     
